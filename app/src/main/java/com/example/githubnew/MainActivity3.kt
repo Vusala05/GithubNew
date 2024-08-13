6316a746b3d32397181a91fda3554bc23a3d58d1
Vusala05 <167974345+Vusala05@users.noreply.github.com>
2024-08-13 10:14:24 +0400

    sharedPreference code is ready in MainActivity3.kt

--- app/src/main/java/com/example/githubnew/MainActivity3.kt
+++ app/src/main/java/com/example/githubnew/MainActivity3.kt
@@ -1,11 +1,36 @@
 package com.example.githubnew
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.githubnew.databinding.ActivityMain3Binding
 
 class MainActivity3 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain3Binding
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+        binding = ActivityMain3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        sharedPreferences = getSharedPreferences("NoteData", Context.MODE_PRIVATE)
+        binding.save.setOnClickListener{
+            val note = binding.notes.text.toString()
+
+            val preferencesEdit = sharedPreferences.edit()
+            preferencesEdit.putString("notes",note)
+            preferencesEdit.apply()
+            Toast.makeText(this,"notes successfully saved", Toast.LENGTH_SHORT).show()
+            binding.notes.text.clear()
+
+        }
+        binding.display.setOnClickListener{
+            val storedData = sharedPreferences.getString("notes"," ")
+            binding.noteText.text = "$storedData"
+        }
+
+
+
     }
 }
