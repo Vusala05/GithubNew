8be10af80e6162879abb04b3166ecd48c1687ee1
Vusala05 <167974345+Vusala05@users.noreply.github.com>
2024-08-13 10:09:53 +0400

    plus and minus operations are ready

--- app/src/main/java/com/example/githubnew/MainActivity.kt
+++ app/src/main/java/com/example/githubnew/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.githubnew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.githubnew.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupPlus()
+        setupMinus()
+
+    }
+    private fun setupPlus() {
+        binding.plus.setOnClickListener {
+            val firstNum = binding.firstNum.text.toString().toDoubleOrNull() ?: 0.0
+            val secondNum = binding.secondNum.text.toString().toDoubleOrNull() ?: 0.0
+            val result = firstNum + secondNum
+            binding.result.text = result.toString()
+        }
+    }
+    private fun setupMinus() {
+        binding.minus.setOnClickListener {
+            val firstNum = binding.firstNum.text.toString().toDoubleOrNull() ?: 0.0
+            val secondNum = binding.secondNum.text.toString().toDoubleOrNull() ?: 0.0
+            val result = firstNum - secondNum
+            binding.result.text = result.toString()
+        }
     }
 }
